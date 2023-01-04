d8880dbd6ba937435be91a5438ed082347d4cf55
Farxodbek Ravshanbekov <62793789+HEllo1234567890-cloud@users.noreply.github.com>
2023-01-04 19:40:10 +0500

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const https = require('https')
+const app = express()
+const corse = require('random-curse-words')
+const bp = require('body-parser')
+const ejs = require('ejs')
+const url = 'https://api.adviceslip.com/advice'
+app.use(bp.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.listen(process.env.PORT||3000,function(){
+    console.log('On it...')
+})
+app.get('/', function(req,res){
+    https.get(url,function(response){
+        response.on('data',function(data){
+            const advice = JSON.parse(data)
+            console.log(advice)
+            var adv = advice.slip.advice
+            var id = advice.slip.id
+            res.render('index',{
+                number_id:'ADVICE #' + id,
+                advice:adv,
+                curse: corse()
+            });
+        })
+    })
+})
+app.post('/',function(req,res){
+    res.redirect('/')
+})
